bcd70d343c2983f64d3c0dd27719854dff27c3da
Ndn1618 <ndilbar1618@gmail.com>
2020-12-01 13:49:40 +0500

    Optimize codes

--- js/script.js
+++ js/script.js
@@ -17,7 +17,6 @@ modalBtn.addEventListener('click', function () {
 
 modalBg.addEventListener('click', function (evt) {
   if (!evt.target.matches('.modal-bg')) {
-    // modalBg.classList.remove('bg-active');
     document.body.removeEventListener('keyup', logKeyCode);
   }
 });
